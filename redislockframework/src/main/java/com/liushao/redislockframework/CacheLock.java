194fbb5f700f0436507db52dedabacff03183ee7
Administrator <Administrator@Admin>
2017-07-16 21:04:09 +0800

    注释

--- redislockframework/src/main/java/com/liushao/redislockframework/CacheLock.java
+++ redislockframework/src/main/java/com/liushao/redislockframework/CacheLock.java
@@ -9,8 +9,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+//cachelock是方法级的注解，用于注解会产生并发问题的方法
 public @interface CacheLock {
 	String lockedPrefix() default "";//redis 锁key的前缀
-	long timeOut() default 2000;//锁时间
+	long timeOut() default 2000;//轮询锁的时间
 	int expireTime() default 100000;//key在redis里存在的时间，1000S
 }
